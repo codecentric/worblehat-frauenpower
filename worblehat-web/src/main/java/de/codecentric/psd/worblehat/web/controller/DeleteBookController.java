2573a7e8b5c6689735ce3c43c7a98a7ae5b87ba0
unknown <asf19@OGC06-133.pk.tm.fh-aachen.de>
2013-03-21 15:05:22 +0100

    bugfix deleteBook

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/DeleteBookController.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/DeleteBookController.java
@@ -1,12 +1,17 @@
 package de.codecentric.psd.worblehat.web.controller;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import de.codecentric.psd.worblehat.domain.Book;
 import de.codecentric.psd.worblehat.domain.BookRepository;
 import de.codecentric.psd.worblehat.web.command.DeleteBookFormData;
 
@@ -37,4 +42,21 @@ public class DeleteBookController {
 		modelMap.put("deleteBookFormData", new DeleteBookFormData());
 	}
 
+	@RequestMapping(method = RequestMethod.POST)
+	public String deleteBook(ModelMap modelMap,
+			@ModelAttribute("deleteBookFormData") DeleteBookFormData formdata,
+			BindingResult result) {
+
+		if (result.hasErrors()) {
+			return "/deleteBook";
+		} else {
+			List<Book> booklist = bookRepository.findBooksByISBN(formdata
+					.getIsbn());
+			bookRepository.deleteBook(booklist.get(0));
+			return "/home";
+		}
+
+		// bookService.returnAllBooksByBorrower(formData.getEmailAddress());
+
+	}
 }
